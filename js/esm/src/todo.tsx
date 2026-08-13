2e52741481cea55b9fc88b7a9be33bbce77bf39e
Adrian Greeve <adrian@moodle.com>
2026-08-13 14:19:11 +0800

    Added useState and useEffect to load todos

--- js/esm/src/todo.tsx
+++ js/esm/src/todo.tsx
@@ -6,9 +6,8 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-import {requireAsync} from '@moodle/lms/core/amd';
+import {useEffect, useState} from 'react';
 import * as Repository from '@moodle/lms/block_workshoptodo/repository';
-
 
 const TodoList = ({todos, rootid}) => (
     <ul className="list-group mb-3 workshop-todo" aria-label="Todo list">
@@ -39,10 +38,19 @@ const TodoList = ({todos, rootid}) => (
 );
 
 const TodoApp = ({rootid}) => {
+    const [todos, setTodos] = useState([]);
+
+    const loadTodos = async () => {
+        setTodos(await Repository.getTodos());
+    };
+
+    useEffect(() => {
+        void loadTodos();
+    }, []);
 
     return (
         <div id={rootid} className="block-workshoptodo-app">
-            {/*<TodoList todos={todos} rootid={rootid} />*/}
+            <TodoList todos={todos} rootid={rootid} />
             <form data-region="add-todo-form">
                 <label className="visually-hidden" htmlFor={`new-todo-${rootid}`}>New todo</label>
                 <div className="input-group">
