1767892a85720104aef881d42861b2ef974bddcd
Adrian Greeve <adrian@moodle.com>
2026-08-13 15:34:52 +0800

    Moved the todolist into its own file as a separate component

--- js/esm/src/todo.tsx
+++ js/esm/src/todo.tsx
@@ -8,46 +8,11 @@
 
 import {useEffect, useState} from 'react';
 import * as Repository from '@moodle/lms/block_workshoptodo/repository';
-
-type TodoListProps = {
-    todos: Todo[];
-    rootid: string;
-    onDelete: (id: number) => Promise<void>;
-    onToggle: (todo: Todo, completed: boolean) => Promise<void>;
-};
+import TodoList from '@moodle/lms/block_workshoptodo/todolist';
 
 type Props = {
     rootid: string;
 }
-
-const TodoList = ({todos, rootid, onDelete, onToggle}: TodoListProps) => (
-    <ul className="list-group mb-3 workshop-todo" aria-label="Todo list">
-        {todos.map((todo) => (
-            <li className="list-group-item d-flex align-items-center justify-content-between">
-                <div className="form-check mb-0">
-                    <input
-                        className="form-check-input"
-                        type="checkbox"
-                        id={`todo-${rootid}-${todo.id}`}
-                        data-action="toggle"
-                        data-todo-id={todo.id}
-                        checked={todo.completed}
-                        onChange={(event) => void onToggle(todo, event.target.checked)}
-                    />
-                    <label
-                        className={`form-check-label ${todo.completed ? 'text-decoration-line-through' : ''}`}
-                        htmlFor={`todo-${rootid}-${todo.id}`}
-                    >
-                        {todo.text}
-                    </label>
-                </div>
-                <button className="btn btn-outline-danger btn-sm" type="button" onClick={() => void onDelete(todo.id)}>
-                    Delete
-                </button>
-            </li>
-        ))}
-    </ul>
-);
 
 const TodoApp = ({rootid}: Props) => {
     const [todos, setTodos] = useState([]);
