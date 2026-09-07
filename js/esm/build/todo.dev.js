74713dab250a6adc8bb16c1c98426c7f25cc1dbc
Adrian Greeve <adrian@moodle.com>
2026-09-07 11:24:12 +0800

    Built js files

--- js/esm/build/todo.dev.js
+++ js/esm/build/todo.dev.js
@@ -10,61 +10,7 @@ import { jsxDEV } from "react/jsx-dev-runtime";
  */
 import { useEffect, useState } from "react";
 import * as Repository from "@moodle/lms/block_workshoptodo/repository";
-const TodoList = /* @__PURE__ */ __name(({ todos, rootid, onDelete, onToggle }) => /* @__PURE__ */ jsxDEV("ul", { className: "list-group mb-3 workshop-todo", "aria-label": "Todo list", children: todos.map((todo) => /* @__PURE__ */ jsxDEV("li", { className: "list-group-item d-flex align-items-center justify-content-between", children: [
-  /* @__PURE__ */ jsxDEV("div", { className: "form-check mb-0", children: [
-    /* @__PURE__ */ jsxDEV(
-      "input",
-      {
-        className: "form-check-input",
-        type: "checkbox",
-        id: `todo-${rootid}-${todo.id}`,
-        "data-action": "toggle",
-        "data-todo-id": todo.id,
-        checked: todo.completed,
-        onChange: (event) => void onToggle(todo, event.target.checked)
-      },
-      void 0,
-      false,
-      {
-        fileName: "public/blocks/workshoptodo/js/esm/src/todo.tsx",
-        lineNumber: 34,
-        columnNumber: 21
-      }
-    ),
-    /* @__PURE__ */ jsxDEV(
-      "label",
-      {
-        className: `form-check-label ${todo.completed ? "text-decoration-line-through" : ""}`,
-        htmlFor: `todo-${rootid}-${todo.id}`,
-        children: todo.text
-      },
-      void 0,
-      false,
-      {
-        fileName: "public/blocks/workshoptodo/js/esm/src/todo.tsx",
-        lineNumber: 43,
-        columnNumber: 21
-      }
-    )
-  ] }, void 0, true, {
-    fileName: "public/blocks/workshoptodo/js/esm/src/todo.tsx",
-    lineNumber: 33,
-    columnNumber: 17
-  }),
-  /* @__PURE__ */ jsxDEV("button", { className: "btn btn-outline-danger btn-sm", type: "button", onClick: () => void onDelete(todo.id), children: "Delete" }, void 0, false, {
-    fileName: "public/blocks/workshoptodo/js/esm/src/todo.tsx",
-    lineNumber: 50,
-    columnNumber: 17
-  })
-] }, void 0, true, {
-  fileName: "public/blocks/workshoptodo/js/esm/src/todo.tsx",
-  lineNumber: 32,
-  columnNumber: 13
-})) }, void 0, false, {
-  fileName: "public/blocks/workshoptodo/js/esm/src/todo.tsx",
-  lineNumber: 30,
-  columnNumber: 5
-}), "TodoList");
+import TodoList from "@moodle/lms/block_workshoptodo/todolist";
 const TodoApp = /* @__PURE__ */ __name(({ rootid }) => {
   const [todos, setTodos] = useState([]);
   const loadTodos = /* @__PURE__ */ __name(async () => {
@@ -96,39 +42,39 @@ const TodoApp = /* @__PURE__ */ __name(({ rootid }) => {
   return /* @__PURE__ */ jsxDEV("div", { id: rootid, className: "block-workshoptodo-app", children: [
     /* @__PURE__ */ jsxDEV(TodoList, { todos, rootid, onDelete: handleDelete, onToggle: handleToggle }, void 0, false, {
       fileName: "public/blocks/workshoptodo/js/esm/src/todo.tsx",
-      lineNumber: 97,
+      lineNumber: 62,
       columnNumber: 13
     }),
     /* @__PURE__ */ jsxDEV("form", { "data-region": "add-todo-form", onSubmit: (event) => void handleFormSubmit(event), children: [
       /* @__PURE__ */ jsxDEV("label", { className: "visually-hidden", htmlFor: `new-todo-${rootid}`, children: "New todo" }, void 0, false, {
         fileName: "public/blocks/workshoptodo/js/esm/src/todo.tsx",
-        lineNumber: 99,
+        lineNumber: 64,
         columnNumber: 17
       }),
       /* @__PURE__ */ jsxDEV("div", { className: "input-group", children: [
         /* @__PURE__ */ jsxDEV("input", { className: "form-control", id: `new-todo-${rootid}`, name: "text", type: "text", placeholder: "New todo", required: true }, void 0, false, {
           fileName: "public/blocks/workshoptodo/js/esm/src/todo.tsx",
-          lineNumber: 101,
+          lineNumber: 66,
           columnNumber: 21
         }),
         /* @__PURE__ */ jsxDEV("button", { className: "btn btn-primary", type: "submit", children: "Add" }, void 0, false, {
           fileName: "public/blocks/workshoptodo/js/esm/src/todo.tsx",
-          lineNumber: 102,
+          lineNumber: 67,
           columnNumber: 21
         })
       ] }, void 0, true, {
         fileName: "public/blocks/workshoptodo/js/esm/src/todo.tsx",
-        lineNumber: 100,
+        lineNumber: 65,
         columnNumber: 17
       })
     ] }, void 0, true, {
       fileName: "public/blocks/workshoptodo/js/esm/src/todo.tsx",
-      lineNumber: 98,
+      lineNumber: 63,
       columnNumber: 13
     })
   ] }, void 0, true, {
     fileName: "public/blocks/workshoptodo/js/esm/src/todo.tsx",
-    lineNumber: 96,
+    lineNumber: 61,
     columnNumber: 9
   });
 }, "TodoApp");
